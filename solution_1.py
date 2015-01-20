1f26fd6e1d1b7698737ce830ad36f76ce5e4ab55
shuaib88 <shuaibahmed@shuaibs-air.attlocal.net>
2015-01-20 10:14:38 -0600

    Homework solution 1
    Chapter 8 number 1 fibonnaci sequence

---
+++ solution_1.py
@@ -0,0 +1,29 @@
+#Program finds the nth fibonnaci number. (Assuming - 1st fib is 1, 2nd is 1, 3rd is two)
+
+
+
+def fibonnaci():
+    n = eval(input("Which nth fibonnaci number would you like to find?? "))
+
+    if n == 0:
+        current_fib = 0
+        
+    elif n == 1:
+        current_fib = 1
+
+    elif n == 2:
+        current_fib = 1
+
+    elif n > 2: 
+
+        prev_fib = 1
+        current_fib = 2
+        
+        for i in range(n-3):
+            holding = current_fib
+            current_fib = prev_fib + current_fib
+            prev_fib = holding
+
+        print(current_fib)
+    
+fibonnaci()
